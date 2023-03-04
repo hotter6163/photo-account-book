e5d16edfb1a0a12fcff5e7c5c1ad1119e8cb3177
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-03-04 16:38:23 +0900

    :coffin: remove default css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -18,7 +17,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -87,5 +86,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
