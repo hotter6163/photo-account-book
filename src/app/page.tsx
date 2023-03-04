eb8a7b747ea9725e807c07406c2ce34bca4bb750
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-03-04 17:14:31 +0900

    :art: yarn format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="w-full h-full flex justify-center items-center">
+    <main className='w-full h-full flex justify-center items-center'>
       <p>hello world</p>
     </main>
-  );
+  )
 }
